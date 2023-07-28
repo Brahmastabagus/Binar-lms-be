7cb255401d2f85f5fb8f703b432b2f372cfb71cc
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-07-28 23:48:16 +0700

    Initial project

---
+++ config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "development": {
+    username: "postgres",
+    password: "12345",
+    database: "db_lms",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+}
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const routes = require('./routes')
+
+const app = express()
+const PORT = 8080
+
+app.use(
+  cors({
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`App running on port: ${PORT}`);
+})
