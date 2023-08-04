e7f89db56b8bfd3a18d10b572a65cdb10ce41d5b
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-08-04 19:15:43 +0700

    chore(sequelize): create models and migration for users and todos table

---
+++ migrations/20230804093934-create-todos.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      desc: {
+        type: Sequelize.STRING
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      priority: {
+        type: Sequelize.ENUM(["high", "medium", "low"]),
+        defaultValue: "medium"
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('todos');
+  }
+};
---
+++ models/users.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.hasMany(models.todos, {
+        foreignKey: "user_id",
+        as: "todos"
+      })
+    }
+  }
+  users.init({
+    name: DataTypes.STRING,
+    number: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'users',
+  });
+  return users;
+};
