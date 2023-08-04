d609c6038c7731c069a0cbce364546ffca465f23
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-08-04 14:21:28 +0700

    chore(tools): setup all tools

--- config/database.js
+++ config/database.js
@@ -1,9 +1,26 @@
 module.exports = {
-  "development": {
-    username: "postgres",
-    password: "12345",
-    database: "db_lms",
-    host: "127.0.0.1",
-    dialect: "postgres"
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: `${process.env.DB_NAME}`,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: `${process.env.DB_NAME}_production`,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: `${process.env.DB_NAME}_test`,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
   },
 }
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
 
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT
 
 app.use(
   cors({
