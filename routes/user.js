655da3fc03ad627f6f035fe98d08c533f1ea42b1
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-08-14 19:35:16 +0700

    fix: testing user auth

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const userController = require('../controllers/userController')
 const auth = require('../middlewares/auth')
 
-router.get("/", auth, userController.getUsers)
+router.get("/", userController.getUsers)
 router.post("/login", userController.login)
 
 module.exports = router
